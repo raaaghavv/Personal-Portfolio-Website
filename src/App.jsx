fd2ebb055b835011c792b1ad9612ca4670e73c22
Raghav Goel <raghavgoel454@gmail.com>
2025-08-01 21:12:12 +0530

    chore: Import all section components from section folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import Hero from "./components/sections/Hero";
+import AboutMe from "./components/sections/AboutMe";
+import Experiences from "./components/sections/Experiences";
+import Projects from "./components/sections/Projects";
+import TechStack from "./components/sections/TechStack";
+import Publications from "./components/sections/Publications";
+import Contact from "./components/sections/Contact";
 
 function App() {
   return (
-    <div>App</div>
-  )
+    <main className="w-[80vw] max-w-[1440px] mx-auto">
+      <Hero />
+      <AboutMe />
+      <Experiences />
+      <Projects />
+      <TechStack />
+      <Publications />
+      <Contact />
+    </main>
+  );
 }
 
-export default App
+export default App;
