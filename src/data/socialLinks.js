582c71a349d4be021281c9e814fb1d0513693db5
Raghav Goel <raghavgoel454@gmail.com>
2025-08-12 14:00:00 +0530

    feat: Create data array for social links

---
+++ src/data/socialLinks.js
@@ -0,0 +1,33 @@
+import {
+  LinkedInIcon,
+  GitHubIcon,
+  XIcon,
+  PeerlistIcon,
+} from "../components/ui/Icons";
+
+export const socialLinks = [
+  {
+    icon: LinkedInIcon,
+    name: "LinkedIn",
+    handle: "raghav-goel01",
+    url: "https://www.linkedin.com/in/raghav-goel01/",
+  },
+  {
+    icon: PeerlistIcon,
+    name: "Peerlist",
+    handle: "raaaghavv",
+    url: "https://peerlist.io/raaaghavv/",
+  },
+  {
+    icon: GitHubIcon,
+    name: "GitHub",
+    handle: "raaaghavv",
+    url: "https://github.com/raaaghavv/",
+  },
+  {
+    icon: XIcon,
+    name: "X",
+    handle: "raaaghavvvvv",
+    url: "https://x.com/raaaghavvvvv/",
+  },
+];
